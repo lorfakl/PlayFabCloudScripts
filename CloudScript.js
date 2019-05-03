0090276fdd8b59a1bfa1ee5d85a2c7297508c837
Jtayl216 <lorfakl@aol.com>
2019-05-03 12:39:56 -0400

    string test

--- CloudScript.js
+++ CloudScript.js
@@ -1,10 +1,10 @@
 handlers.ReturnDictionary = function (args, context)
 {    
-  var dict = {};
+  var dict = "a string";
 
-      dict["one"] = 1;
+      //dict["one"] = 1;
 
-      dict["two"] = 2;
+      //dict["two"] = 2;
 
   return dict;
 
