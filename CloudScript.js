785902067dd0b84de002573cfbba0097f3a145aa
Jtayl216 <lorfakl@aol.com>
2019-05-03 12:55:00 -0400

    added JSONify

--- CloudScript.js
+++ CloudScript.js
@@ -2,10 +2,11 @@ handlers.ReturnDictionary = function (args, context)
 {    
   var dict = "a string";
 
-      //dict["one"] = 1;
+      dict["one"] = 1;
 
-      //dict["two"] = 2;
+      dict["two"] = 2;
 
+  JSON.stringify(dict);
   return dict;
 
 }
