320a619ef8b6e5b5dfa21d09f4246c2d33519f6c
Jtayl216 <lorfakl@aol.com>
2019-05-16 17:58:44 -0400

    doing mmore tests

--- CloudScript.js
+++ CloudScript.js
@@ -1,6 +1,22 @@
-handlers.ReturnDictionary = function (args, context)
-{    
-  var dict = 4
-  return dict;
+// Should be invoked from a task that runs over certain segment
+handlers.PopulateLeaderboard = (args,ctx) => {
+    // When handler is executed as a task over the segment
+    // we can extract individual player id using the next line:
+    let playerId = ctx.playerProfile.PlayerId;
 
+    // Use player id and update player statistics as follows:
+    server.UpdatePlayerStatistics({
+        PlayFabId : playerId,
+        Statistics : [
+            {
+                "StatisticName": "TestScore",
+                "Value": getRandomInRange(100,1000)
+            }
+        ]
+    });
+}
+
+// Utility method to generate random number
+let getRandomInRange = (min, max) => {
+    return Math.round(Math.random() * (max - min) + min);
 }
